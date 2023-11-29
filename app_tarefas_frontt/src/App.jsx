1d8897caa92730e111d1fd386ba69e53ccff953c
dacasfilipe <filiperdacas@gmail.com>
2023-11-29 20:11:03 -0300

    updated

--- app_tarefas_frontt/src/App.jsx
+++ app_tarefas_frontt/src/App.jsx
@@ -1,15 +1,13 @@
 //import './App.css';
 //import MenuSuperior from './components/MenuSuperior';
-import InclusaoLivros from './components/InclusaoLivros';
+import cadastrar_tarefas from './components/cadastrar_tarefa';
 import {Routes,Route} from 'react-router-dom';
-//import ManutencaoLivros from './components/ManutencaoLivros';
-//import ResumoLivros from './components/ResumoLivros';
+
 const App = () => {
   return(  //tudo que vai aqui no return é o que aparece na aplicação
     <>
       <Routes>
-        <Route path="/" element={<InclusaoLivros/>}/>
-        
+        <Route path="/" element={<cadastrar_tarefas/>}/>
       </Routes>
     </>
   )
