712e80ddae7808992daecd7947c83fe16ac88397
dacasfilipe <filiperdacas@gmail.com>
2023-12-05 22:01:54 -0300

    05-12-23

--- app_tarefas_frontt/src/App.jsx
+++ app_tarefas_frontt/src/App.jsx
@@ -1,5 +1,6 @@
 import cadastrar_tarefas from './components/cadastrar_tarefa';
 import {BrowserRouter as Router,Route, Routes} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return(  //tudo que vai aqui no return é o que aparece na aplicação
--- app_tarefas_frontt/src/components/cadastrar_tarefa.jsx
+++ app_tarefas_frontt/src/components/cadastrar_tarefa.jsx
@@ -1,71 +1,100 @@
-//Componente para incluir livros no banco de dados
-//declaração da função do componente IncluirLivros
 import { useForm } from "react-hook-form";
 import { api } from "../config_axios";
 import { useState } from "react";
-//register serve para definir os nomes dos campos do form (validações)
-//handleSubmit, para indicar o método a ser acionado no evento onSubmit do form
-//form onSubmit={handleSubmit(salvar)}
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const Cadastrar_tarefa = () => {
-    const{ register, handleSubmit } = useForm();
-    const [aviso, setAviso] = useState("");
-    //método chamado ao enviar form onSubmit
-    const salvar = async (campos) => {  
-        try {
-                    const response = await api.post("tarefa", campos);
-                    setAviso(`Tarefa cadastrada com sucesso!"
-                    ${response.data.id}`);
-                } catch (error) {
-                    setAviso("Erro ao cadastrar tarefa!");
-                }
+  const { register, handleSubmit } = useForm();
+  const [aviso, setAviso] = useState("");
+
+  const salvar = async (campos) => {
+    try {
+      const response = await api.post("tarefa", campos);
+      setAviso(`Tarefa cadastrada com sucesso!" ${response.data.id}`);
+    } catch (error) {
+      setAviso("Erro ao cadastrar tarefa!");
     }
-        
-    //aqui é o que vai ser exibido em tela
-    return ( 
-        <div className="container">
-            <h4 className="fst-italic mt-3">Cadastrar Tarefa</h4>
-            <form onSubmit={handleSubmit(salvar)}>
-                <div className="form-group">
-                    <label htmlFor="titulo">Titulo</label>
-                    <input type="text" className="form-control" id="titulo"
-                    required autoFocus {...register("titulo")}/>
-                </div>
-                <div className="form-group mt-2">
-                    <label htmlFor="descricao">Descrição</label>
-                        <input type="text" className="form-control" id="descricao"
-                        required {...register("descricao")}/>
-                </div>
-                <div className="form-group mt-2">
-                    <label htmlFor="status">Status:</label>
-                        <input type="url" className="form-control" id="status"
-                        required {...register("status")}/>
-                </div>
-                <div className="row mt-2">
-                    <div className="col-sm-4">
-                        <div className="form-group">
-                            <label htmlFor="data_criacao">Data de Criação</label>
-                            <input type="date" className="form-control"
-                            id="data_criacao" required {...register("data_criacao")}></input>
-                        </div>
-                    </div>
-                </div>
-                <div className="row mt-2">
-                    <div className="col-sm-4">
-                        <div className="form-group">
-                            <label htmlFor="data_limite">Data Limite</label>
-                            <input type="date" className="form-control"
-                            id="data_limite" required {...register("data_limite")}></input>
-                        </div>
-                    </div>
-                </div>
-                <input type="submit" className="btn btn-primary mt-3"
-                value="Enviar" />
-                <input type="reset" className="btn btn-danger mt-3 ms-3"
-                value="Limpar"/>
+  };
+
+  return (
+    <div className="container-fluid bg-dark text-light min-vh-100 d-flex align-items-center">
+      <div className="container p-5 bg-light text-dark rounded">
+        <h4 className="fst-italic mb-3">Cadastrar Tarefa</h4>
+        <form onSubmit={handleSubmit(salvar)}>
+          <div className="form-group">
+            <label htmlFor="titulo">Titulo</label>
+            <input
+              type="text"
+              className="form-control"
+              id="titulo"
+              required
+              autoFocus
+              {...register("titulo")}
+            />
+          </div>
+          <div className="form-group mt-2">
+            <label htmlFor="descricao">Descrição</label>
+            <input
+              type="text"
+              className="form-control"
+              id="descricao"
+              required
+              {...register("descricao")}
+            />
+          </div>
+          <div className="form-group mt-2">
+            <label htmlFor="status">Status:</label>
+            <input
+              type="text"
+              className="form-control"
+              id="status"
+              required
+              {...register("status")}
+            />
+          </div>
+          <div className="row mt-2">
+            <div className="col-sm-4">
+              <div className="form-group">
+                <label htmlFor="data_criacao">Data de Criação</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  id="data_criacao"
+                  required
+                  {...register("data_criacao")}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="row mt-2">
+            <div className="col-sm-4">
+              <div className="form-group">
+                <label htmlFor="data_limite">Data Limite</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  id="data_limite"
+                  required
+                  {...register("data_limite")}
+                />
+              </div>
+            </div>
+          </div>
+          <input
+            type="submit"
+            className="btn btn-primary mt-3"
+            value="Enviar"
+          />
+          <input
+            type="reset"
+            className="btn btn-danger mt-3 ms-3"
+            value="Limpar"
+          />
         </form>
-        <div className="alert"></div>
-        </div>
-    )
-}
+        <div className="alert mt-3">{aviso}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Cadastrar_tarefa;
