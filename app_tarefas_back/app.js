3187eb456f8690aaa858fe6200e120228130846b
dacasfilipe <filiperdacas@gmail.com>
2023-12-06 21:13:16 -0300

    fixed route at the backend /tarefa to /tarefas

--- app_tarefas_back/app.js
+++ app_tarefas_back/app.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/usuarios', usuarioRouter);
-app.use('/tarefa', tarefaRouter);
+app.use('/tarefas', tarefaRouter);
 
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
