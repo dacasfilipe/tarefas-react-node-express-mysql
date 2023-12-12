9ada8bf6a8d81981677d1d0168f8c7326dd3b1c8
dacasfilipe <filiperdacas@gmail.com>
2023-12-12 11:07:25 -0300

    inserted authprovider

--- app_tarefas_frontt/src/App.jsx
+++ app_tarefas_frontt/src/App.jsx
@@ -2,35 +2,26 @@ import Cadastrar_Tarefas from './components/cadastrar_tarefa';
 import Menu_Superior from './components/MenuSuperior';
 import Manutencao_Tarefas from './components/manutencao_tarefas';
 import FormularioLogin from './components/login';
-
-import { useState } from "react";
 import {BrowserRouter as Router,Route, Routes} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AuthProvider } from './components/AuthProvider'; // Caminho para o AuthProvider
 
 const App = () => {
-  //autenticação
-  const [autenticado, setAutenticado] = useState(false);
-  if (autenticado){
-  return(  //tudo que vai aqui no return é o que aparece na aplicação
-    <>
+    return (
+      <>
+      <AuthProvider>
+  
       <Router>
         <Menu_Superior/>
       <Routes>
-        <Route exact path="/" Component={Cadastrar_Tarefas}/>
-        <Route exact path="/manutencao" Component={Manutencao_Tarefas}/>
-                
+        <Route path="/" element={<Cadastrar_Tarefas />}/>
+        <Route path="/manutencao" element={<Manutencao_Tarefas />}/>
+        <Route path="/login" element={<FormularioLogin />}/>       
       </Routes>
       </Router>
+      </AuthProvider>
     </>
   );
-  } else{
-    // Usuário não autenticado
-    return (
-      <>
-        <FormularioLogin/>
-      </>
-    );
-  }
+}        
 
-};
 export default App;
---
+++ app_tarefas_frontt/src/components/AuthProvider.jsx
@@ -0,0 +1,23 @@
+import { useState, createContext, useContext } from 'react';
+
+const AuthContext = createContext(null);
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+    const [autenticado, setAutenticado] = useState(false);
+
+    const login = () => {
+        setAutenticado(true);
+    };
+
+    const logout = () => {
+        setAutenticado(false);
+    };
+
+    return (
+        <AuthContext.Provider value={{ autenticado, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
